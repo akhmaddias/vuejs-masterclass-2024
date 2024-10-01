e0c49f428de28604d15b78c8c902f08397ad4b40
Akhmad Dias <akhmad.dias94@gmail.com>
2024-10-01 18:52:31 +0500

    add supabase types

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from 'database/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) console.log(error)
+
+  projects.value = data
+
+  console.log('Projects...', projects)
+})()
+</script>
 
 <template>
   <div>
     <h1>Home Page</h1>
     <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Project 1</RouterLink>
+    {{ projects }}
   </div>
 </template>
