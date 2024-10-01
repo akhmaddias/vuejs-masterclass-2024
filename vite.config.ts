98621a05be2d3cd8209171b97375acbe5dcc906d
Akhmad Dias <akhmad.dias94@gmail.com>
2024-10-01 20:03:27 +0500

    add shadcn vue

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [VueRouter(), vue()],
   resolve: {
     alias: {
