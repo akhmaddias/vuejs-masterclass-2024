94ee80f78c2b5d904242e5c5dc46437f3d294ed8
Akhmad Dias <akhmad.dias94@gmail.com>
2024-10-02 18:29:32 +0500

    add unplugin-auto-imports

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import VueRouter from 'unplugin-vue-router/vite'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
@@ -22,6 +25,17 @@ export default defineConfig({
           isCustomElement: (element) => element.startsWith('iconify-icon')
         }
       }
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: ['vue', VueRouterAutoImports],
+      dts: true,
+      viteOptimizeDeps: true
     })
   ],
   resolve: {
