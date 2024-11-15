5c91c0bf7fdb62d5b4e3fbda4e4a1322aef9a1ab
Akhmad Dias <akhmad.dias94@gmail.com>
2024-11-16 01:30:25 +0500

    add collabs composable

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useCollabs } from '@/composables/collabs'
 import { columns } from '@/utils/tableColumns/projectColumns'
 
 usePageStore().pageData.title = 'Projects'
@@ -6,6 +7,7 @@ usePageStore().pageData.title = 'Projects'
 const projectsLoader = useProjectsStore()
 const { projects } = storeToRefs(projectsLoader)
 const { getProjects } = projectsLoader
+const { getProfilesByIds } = useCollabs()
 
 await getProjects()
 </script>
