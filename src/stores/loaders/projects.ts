7a842dedfce83fc532546e0f75d773d589c146bf
Akhmad Dias <akhmad.dias94@gmail.com>
2024-11-16 00:50:40 +0500

    add cache validation

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -2,16 +2,31 @@ import { projectsQuery, type Projects } from '@/utils/supaQueries'
 import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('projects-store', () => {
-  const projects = ref<Projects | null>(null)
+  const projects = ref<Projects>()
 
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
+
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data, error }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          return
+        } else {
+          loadProjects.delete('projects')
+          if (!error && data) projects.value = data
+        }
+      })
+    }
+  }
 
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
 
-    projects.value = data
+    if (data) projects.value = data
+
+    validateCache()
   }
   return {
     projects,
