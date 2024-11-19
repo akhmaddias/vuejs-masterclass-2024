38ed0a625a157e1ce2be04f7b5eab6bb684c8392
Akhmad Dias <akhmad.dias94@gmail.com>
2024-11-20 03:06:24 +0500

    emit project update on status change

--- src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditStatus.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 const value = defineModel<'in-progress' | 'completed'>()
 
-defineEmits(['commit'])
+const emit = defineEmits(['commit'])
+
+const toggleValue = () => {
+  value.value = value.value === 'completed' ? 'in-progress' : 'completed'
+  emit('commit')
+}
 </script>
 
 <template>
-  <div class="text-2xl cursor-pointer">
+  <div class="text-2xl cursor-pointer" @click="toggleValue">
     <iconify-icon v-if="value === 'completed'" class="text-green-500" icon="lucide:circle-check" />
     <iconify-icon v-else class="text-gray-500" icon="lucide:circle-dot" />
   </div>
