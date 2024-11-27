051d8cc5eb7dc14d7b3a5de49679a488034d85ff
Akhmad Dias <akhmad.dias94@gmail.com>
2024-11-27 21:45:29 +0500

    add dynamic collabs in project page

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -13,6 +13,12 @@ watch(
 )
 
 await getProject(slug)
+
+const { getProfilesByIds } = useCollabs()
+const collabs = project.value?.collaborators
+  ? await getProfilesByIds(project.value?.collaborators)
+  : []
+console.log(collabs)
 </script>
 
 <template>
@@ -41,11 +47,14 @@ await getProject(slug)
         <div class="flex">
           <Avatar
             class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            v-for="collab in project.collaborators"
-            :key="collab"
+            v-for="collab in collabs"
+            :key="collab.id"
           >
-            <RouterLink class="w-full h-full flex items-center justify-center" to="">
-              <AvatarImage src="" alt="" />
+            <RouterLink
+              class="w-full h-full flex items-center justify-center"
+              :to="{ name: '/users/[username]', params: { username: collab.username } }"
+            >
+              <AvatarImage :src="collab.avatar_url || ''" alt="" />
               <AvatarFallback> </AvatarFallback>
             </RouterLink>
           </Avatar>
