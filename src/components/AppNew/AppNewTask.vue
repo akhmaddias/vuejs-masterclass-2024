c3c3c53b3a21d4b1e3de2bbf110af61bdabecb6d
Akhmad Dias <akhmad.dias94@gmail.com>
2024-12-02 23:16:12 +0500

    add new task component

---
+++ src/components/AppNew/AppNewTask.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const sheetOpen = defineModel<boolean>()
+
+defineEmits(['commit'])
+</script>
+
+<template>
+  <Sheet v-model:open="sheetOpen">
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Create New Task</SheetTitle>
+      </SheetHeader>
+      <p>Content</p>
+    </SheetContent>
+  </Sheet>
+</template>
