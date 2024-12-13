eae9016e350ede1c1d2dee99b9383bc344d91523
Akhmad Dias <akhmad.dias94@gmail.com>
2024-12-14 01:54:51 +0500

    use provide and inject for menu open

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { menuKey } from '@/utils/injectionKeys'
 import { useWindowSize } from '@vueuse/core'
+
+import type { MenuInjectionOptions } from '@/utils/injectionKeys'
 
 const { profile } = storeToRefs(useAuthStore())
 
@@ -47,7 +50,7 @@ const executeAction = async (linkTitle: string) => {
 }
 defineEmits(['taskClicked'])
 
-const { menuOpen, toggleMenu } = useMenu()
+const { menuOpen, toggleMenu } = inject(menuKey) as MenuInjectionOptions
 const windowWidth = useWindowSize().width
 
 watchEffect(() => {
